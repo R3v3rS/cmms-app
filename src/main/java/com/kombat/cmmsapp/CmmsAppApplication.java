6400c950857f2dcd1deed9137ba88aa92e9c626d
R3v3rS <pepsi0192@gmail.com>
2023-07-01 11:45:23 +0200

    Utworzenie projektu

---
+++ src/main/java/com/kombat/cmmsapp/CmmsAppApplication.java
@@ -0,0 +1,13 @@
+package com.kombat.cmmsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmmsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmmsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kombat/cmmsapp/CmmsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kombat.cmmsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmmsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
